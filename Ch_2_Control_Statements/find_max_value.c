1a2d127822cf42157e40eb1e9cc6a0eff52f41fc
priyakp07 <priyakp080@gmail.com>
2023-09-04 23:11:38 +0530

    fixed error in the find maximum value

--- Ch_2_Control_Statements/find_max_value.c
+++ Ch_2_Control_Statements/find_max_value.c
@@ -1,32 +1,32 @@
 // find maximum value
 
-#include<stdio.h>
+#include <stdio.h>
 
 int main(int argc, char const *argv[])
 {
     int a, b, c, max;
-    printf("Enter two numbers:  ");
-    scanf("%d %d %d" , &a, &b, &c);
-    if (a>b)
+    printf("Enter three numbers:  ");
+    scanf("%d %d %d", &a, &b, &c);
+    if (a > b)
     {
-       if(a>c)
-       {
-           max=a;
-       }
-       else
-       {
-        max=c;
-       }
-    }
-    else
-    {
-        if(b>c)
+        if (a > c)
         {
-            max=b;
+            max = a;
         }
         else
         {
-            max=b;
+            max = c;
+        }
+    }
+    else
+    {
+        if (b > c)
+        {
+            max = b;
+        }
+        else
+        {
+            max = c;
         }
     }
     printf("maximum value is %d", max);
