0bcb8b9070f690a838ba8b418ddb7d80136699d4
priyakp07 <priyakp080@gmail.com>
2023-09-04 23:22:15 +0530

    introduction to c programing project practicle

--- Ch1_C_Introduction/interchange_two_number.c
+++ Ch1_C_Introduction/interchange_two_number.c
@@ -10,6 +10,6 @@ int main(int argc, char const *argv[])
     a=a+b;
     b=a-b;
     a=a-b;
-    printf("\nAfter interchange value A = %d  B = %d" , a, b);
+    printf("\nAfter interchange value A = %d , B = %d" , a, b);
     return 0;
 }
