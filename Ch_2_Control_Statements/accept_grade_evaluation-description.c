6a66ccff317ca45c7010997e88ab2dcab9ace341
priyakp07 <priyakp080@gmail.com>
2023-09-10 22:33:12 +0530

    switch case statement practice program

---
+++ Ch_2_Control_Statements/accept_grade_evaluation-description.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    char a;
+    printf("input the grade: ");
+    scanf("%c",&a);
+    switch (a)
+    {
+    case 'E':
+      printf("Exellent");
+        break;
+    case 'V':
+      printf("very good");
+        break;
+    case 'F':
+      printf("fail");
+        break;
+    case 'G':
+      printf("Good");
+        break;
+    case 'A':
+      printf("Average");
+        break;
+    }
+    return 0;
+}
