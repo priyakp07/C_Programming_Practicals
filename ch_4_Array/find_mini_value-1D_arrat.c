0ccadfceede32720b1bc0d23a6fe7068eff9f26d
priyakp07 <priyakp080@gmail.com>
2023-09-12 22:05:27 +0530

    find minimum value program

---
+++ ch_4_Array/find_mini_value-1D_arrat.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a[50], n, i, mini;
+    printf("How many number you find minimum value");
+    scanf("%d", &n);
+    printf("Enter value");
+    for ( i = 0; i < n; i++)
+    {
+        scanf("%d" , &a[i]);
+        if (i==0)
+        {
+            mini=a[i];
+        }
+        else
+        {
+            if (a[i]<mini)
+            {
+                mini=a[i];
+            }
+        }
+    }
+        printf("minimum value is = %d",mini);
+    return 0;
+}
